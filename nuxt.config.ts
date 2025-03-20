1f2ce06e20552e751b74572de483f90a8367e62d
Rafail Miliopoulos <rafail.miliopoulos@nl.abnamro.com>
2025-03-21 00:28:46 +0100

    applying config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,56 +4,53 @@ import Aura from "@primeuix/themes/aura";
 import PrimeUI from "tailwindcss-primeui";
 
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
 
   modules: [
-    '@nuxt/content',
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxt/icon',
-    '@nuxt/image',
-    '@nuxt/scripts',
-    '@nuxt/test-utils',
-    '@nuxtjs/tailwindcss',
+    "@nuxt/content",
+    "@nuxt/eslint",
+    "@nuxt/image",
+    "@nuxt/scripts",
+    "@nuxt/test-utils",
+    "@nuxtjs/tailwindcss",
     "@primevue/nuxt-module",
-    '@nuxthq/studio'
   ],
 
   primevue: {
     options: {
-        theme: {
-            preset: Aura,
-            options: {
-                darkModeSelector: ".p-dark",
-                cssLayer: {
-                    name: 'primevue',
-                    order: 'theme, primevue'
-                }
-            },
+      theme: {
+        preset: Aura,
+        options: {
+          darkModeSelector: ".p-dark",
+          cssLayer: {
+            name: "primevue",
+            order: "theme, primevue",
+          },
         },
-        ripple: true,
+      },
+      ripple: true,
     },
     autoImport: true,
+  },
 
-    content: {
-        preview: {
-          api: 'https://api.nuxt.studio',
-          gitInfo: {
-            name: 'nuxt3-styled-tailwind',
-            owner: 'rafaelmils',
-            url: 'https://github.com/rafaelmils/nuxt3-styled-tailwind'
-          }
-        }
-      }
-},
+  content: {
+    preview: {
+      api: "https://api.nuxt.studio",
+      gitInfo: {
+        name: "nuxt3-styled-tailwind",
+        owner: "rafaelmils",
+        url: "https://github.com/rafaelmils/nuxt3-styled-tailwind",
+      },
+    },
+  },
 
-css: ["primeicons/primeicons.css"],
+  css: ["primeicons/primeicons.css"],
 
   tailwindcss: {
-      config: {
-          plugins: [PrimeUI],
-          darkMode: ["class", ".p-dark"],
-      },
+    config: {
+      plugins: [PrimeUI],
+      darkMode: ["class", ".p-dark"],
+    },
   },
-})
+});
